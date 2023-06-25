8cea87e500dd237737ab2935671f9d70654bde8f
crococo001 <126978444+crococo001@users.noreply.github.com>
2023-06-25 17:30:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php print("Hello World")?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My First PHP Project";
+
+echo '<br>';
+
+echo "About me";
+
+echo '<br>';
+echo '<a href="https://www.apkcroc.com">apk croc</a>';
+?>
+
+
+
+</body>
+</html>
